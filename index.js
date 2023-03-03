aaff97f1b8bba61b4b26acaf14fa6a4e8c9b9f5f
dntcuervo <dastringa03@gmail.com>
2023-03-02 23:33:31 -0300

    02/03

--- index.js
+++ index.js
@@ -15,9 +15,20 @@ function navHighlighter() {
       scrollY > sectionTop &&
       scrollY <= sectionTop + sectionHeight
     ){
-      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
+      document.querySelector("nav a[href*=" + sectionId + "]").classList.add("active");
     } else {
-      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
+      document.querySelector("nav a[href*=" + sectionId + "]").classList.remove("active");
     }
   });
 }
+
+window.addEventListener('scroll', function() {
+  var navbar = document.querySelector('.scroll-nav');
+  if (window.pageYOffset > 50) {
+    navbar.classList.remove('nav-claro');
+    navbar.classList.add('nav-oscuro');
+  } else {
+    navbar.classList.remove('nav-oscuro');
+    navbar.classList.add('nav-claro');
+  }
+});
